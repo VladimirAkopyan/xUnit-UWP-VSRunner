e2502004d5a602523809967fae109b6a05549b33
Vladimir Akopyan <vlad@quickbird.uk>
2016-03-02 22:09:05 +0000

    initial

---
+++ MyFirstUWPTests/Other.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyFirstUWPTests
+{
+    public class Other
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        [Theory]
+        [InlineData(3, 2.5)]
+        public void AnotherTheory(int x, float m)
+        {
+            float j = (float)x;
+            m += x;
+
+            Assert.True(m < 6); 
+        }
+
+    }
+}
